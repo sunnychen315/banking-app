0bb813184026dd7b9817fb540b862c851310df24
Sunny Chen <sunny.m.chen3@gmail.com>
2021-04-23 20:05:12 -0700

    created Account class and testing if files are committed

---
+++ src/edu/sjsu/model/Account.java
@@ -0,0 +1,26 @@
+package edu.sjsu.model;
+
+public class Account {
+    double balance;
+    double amountToWithdraw;
+    double amountToDeposit;
+
+    public Account(double balance, double amountToWithdraw, double amountToDeposit) {
+        this.balance = balance;
+        this.amountToWithdraw = amountToWithdraw;
+        this.amountToDeposit = amountToDeposit;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public double getAmountToWithdraw() {
+        return this.amountToWithdraw;
+    }
+
+    public double getAmountToDeposit() {
+        return this.amountToDeposit;
+    }
+
+}
