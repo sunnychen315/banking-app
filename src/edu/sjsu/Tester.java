15391d6b6e44e15d5cc1d6e1705751048d574fc5
abey-john <abey.john@sjsu.edu>
2021-04-24 16:58:57 -0700

    Testing commit

--- src/edu/sjsu/Tester.java
+++ src/edu/sjsu/Tester.java
@@ -4,5 +4,6 @@ public class Tester {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        System.out.println("Part 2 of testing git commit"); // Abey
     }
 }
