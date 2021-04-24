b2a9232af0b27993ca7b8f8124eee59c0fbe1cb4
Sunny Chen <sunny.m.chen3@gmail.com>
2021-04-23 20:18:08 -0700

    added javadoc comments to Account class

--- src/edu/sjsu/model/Account.java
+++ src/edu/sjsu/model/Account.java
@@ -1,28 +1,50 @@
 package edu.sjsu.model;
 
+/**
+ * Class used to store account information
+ */
 public class Account {
     double balance;
     double amountToWithdraw;
     double amountToDeposit;
 
+    /**
+     * Constructor to update variables
+     *
+     * @param balance
+     * @param amountToWithdraw
+     * @param amountToDeposit
+     */
     public Account(double balance, double amountToWithdraw, double amountToDeposit) {
         this.balance = balance;
         this.amountToWithdraw = amountToWithdraw;
         this.amountToDeposit = amountToDeposit;
     }
 
+    /**
+     * @return balance
+     */
     public double getBalance() {
         return this.balance;
     }
 
+    /**
+     * @return amount to withdraw
+     */
     public double getAmountToWithdraw() {
         return this.amountToWithdraw;
     }
 
+    /**
+     * @return amount to deposit
+     */
     public double getAmountToDeposit() {
         return this.amountToDeposit;
     }
 
+    /**
+     * updates balance after withdraw
+     */
     public void withdraw() {
         balance = balance - this.getAmountToWithdraw();
     }
