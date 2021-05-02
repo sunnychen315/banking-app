0d44ad13647b13c5a2475df2e213bdda014686da
abey-john <abey.john@sjsu.edu>
2021-05-01 22:05:19 -0700

    removed account textfield from BankViewer and added it to AccountViewer

--- src/edu/sjsu/view/AccountViewer.java
+++ src/edu/sjsu/view/AccountViewer.java
@@ -1,5 +1,10 @@
 package edu.sjsu.view;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 public class AccountViewer extends BankViewer {
 
     // instance variables
@@ -9,5 +14,31 @@ public class AccountViewer extends BankViewer {
         // Sets the characteristics of the JFrame
         super();
         this.setTitle("Account Page");
+
+        JLabel accountName = new JLabel("Account Name");
+        accountName.setFont(new Font("Sans Serif", Font.BOLD, 30));
+        accountName.setBounds(3 * (this.getWidth() / 4), this.getY() - 30, (this.getWidth() / 4) - 10, this.getHeight() / 6);
+        accountName.setBackground(new Color(160, 212, 226));
+        accountName.setForeground(Color.WHITE);
+        accountName.setHorizontalAlignment(SwingConstants.RIGHT);
+        accountName.setVerticalAlignment(SwingConstants.BOTTOM);
+        accountName.setVisible(true);
+
+        accountName.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                signOut();
+            }
+        });
+
+        lp.add(accountName, 0);
+    }
+
+    public void signOut() {
+        int input = JOptionPane.showConfirmDialog(null, "Do you wish to sign out?", "Sign Out", JOptionPane.YES_NO_OPTION);
+        if (input == 0) {
+            LoginViewer loginViewer = new LoginViewer();
+        } else {
+            // do nothing
+        }
     }
 }
--- src/edu/sjsu/view/BankViewer.java
+++ src/edu/sjsu/view/BankViewer.java
@@ -2,8 +2,12 @@ package edu.sjsu.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class BankViewer extends JFrame {
+
+    JLayeredPane lp;
 
     public BankViewer() {
 
@@ -14,7 +18,7 @@ public class BankViewer extends JFrame {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         this.setSize((int) screenSize.getWidth(), (int) screenSize.getHeight());
 
-        JLayeredPane lp = new JLayeredPane();
+        lp = new JLayeredPane();
         lp.setBounds(this.getX(), this.getY(), this.getWidth(), this.getHeight() / 6);
 
         TopBar bar = new TopBar(this.getX(), this.getY(), this.getWidth(), this.getHeight() / 6);
@@ -26,23 +30,19 @@ public class BankViewer extends JFrame {
 
         JLabel bankName = new JLabel("Bank Name");
         bankName.setFont(new Font("Sans Serif", Font.BOLD, 50));
-        bankName.setBounds(this.getX() + 10, this.getY() - 30, this.getWidth(), this.getHeight() / 6);
+        bankName.setBounds(this.getX() + 10, this.getY() - 30, this.getWidth() / 4, this.getHeight() / 6);
         bankName.setBackground(new Color(160, 212, 226));
         bankName.setForeground(Color.WHITE);
         bankName.setHorizontalAlignment(SwingConstants.LEFT);
         bankName.setVerticalAlignment(SwingConstants.BOTTOM);
         bankName.setVisible(true);
 
-        JLabel accountName = new JLabel("Account Name");
-        accountName.setFont(new Font("Sans Serif", Font.BOLD, 30));
-        accountName.setBounds(this.getX(), this.getY() - 30, this.getWidth() - 10, this.getHeight() / 6);
-        accountName.setBackground(new Color(160, 212, 226));
-        accountName.setForeground(Color.WHITE);
-        accountName.setHorizontalAlignment(SwingConstants.RIGHT);
-        accountName.setVerticalAlignment(SwingConstants.BOTTOM);
-        accountName.setVisible(true);
+        bankName.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                // CONSTRUCT A HOME VIEWER
+            }
+        });
 
-        lp.add(accountName);
         lp.add(bankName);
         lp.add(topBar);
         this.add(lp);
