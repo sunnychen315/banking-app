4180146ce5bca8a0c06cabc5d0decb6bd7e9ca63
abey-john <abey.john@sjsu.edu>
2021-05-04 14:16:47 -0700

    added 'My Accounts' text

--- src/edu/sjsu/view/HomeViewer.java
+++ src/edu/sjsu/view/HomeViewer.java
@@ -1,5 +1,8 @@
 package edu.sjsu.view;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class HomeViewer extends BankViewer {
 
     // instance variables
@@ -11,6 +14,18 @@ public class HomeViewer extends BankViewer {
         this.setTitle("Home Page");
 
         addAccountName();
-        
+
+        JLabel accountText = new JLabel("My Accounts");
+        accountText.setFont(new Font("Sans Serif", Font.BOLD, 20));
+        accountText.setBounds(this.getX() + 10, topPanel.getHeight() - 30, this.getWidth() / 4, this.getHeight() / 10);
+        accountText.setBackground(new Color(7, 63, 120));
+        accountText.setForeground(Color.WHITE);
+        accountText.setHorizontalAlignment(SwingConstants.LEFT);
+        accountText.setVerticalAlignment(SwingConstants.BOTTOM);
+        accountText.setVisible(true);
+
+        this.add(accountText);
+
+        this.setVisible(true);
     }
 }
