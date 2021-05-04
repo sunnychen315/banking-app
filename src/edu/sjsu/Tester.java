40f6d1897c20e890d6ba30681fffa27c42bd832e
abey-john <abey.john@sjsu.edu>
2021-05-03 22:39:28 -0700

    altered tester

--- src/edu/sjsu/Tester.java
+++ src/edu/sjsu/Tester.java
@@ -1,9 +1,9 @@
 package edu.sjsu;
 
-import edu.sjsu.view.LoginViewer;
+import edu.sjsu.view.AccountViewer;
 
 public class Tester {
     public static void main(String[] args) {
-        new LoginViewer();
+        new AccountViewer();
     }
 }
