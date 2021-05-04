3eee035db563d2a44c271076829dbb66f1e7add3
abey-john <abey.john@sjsu.edu>
2021-05-04 15:51:38 -0700

    added comments to tester

--- src/edu/sjsu/Tester.java
+++ src/edu/sjsu/Tester.java
@@ -3,7 +3,10 @@ package edu.sjsu;
 import edu.sjsu.view.AccountViewer;
 
 public class Tester {
+
     public static void main(String[] args) {
         new AccountViewer();
+        // Click "account name" to go to login page
+        // Click "bank name" to go to home page
     }
 }
