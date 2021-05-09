bc63038b519800501d1d31db0971f24f2d84a799
abey-john <abey.john@sjsu.edu>
2021-05-08 18:02:34 -0700

    modified tester

--- src/edu/sjsu/Tester.java
+++ src/edu/sjsu/Tester.java
@@ -3,7 +3,6 @@ package edu.sjsu;
 import edu.sjsu.controller.BankController;
 import edu.sjsu.messages.Message;
 import edu.sjsu.model.UserList;
-import edu.sjsu.view.AccountViewer;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -12,10 +11,6 @@ public class Tester {
 
     public static void main(String[] args) {
         BlockingQueue<Message> queue = new LinkedBlockingQueue<>();
-        new AccountViewer(queue);
-        // new CheckingsViewer(queue);
-        // Click "account name" to go to login page
-
         UserList users = new UserList();
         new BankController(queue, users);
     }
